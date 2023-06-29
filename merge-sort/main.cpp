3615f5d8f38b77a392e8bbaa730f10353cd78236
ATV <alfredotorresvelarde@hotmail.com>
2023-06-28 17:07:39 -0700

    Update - MergeSort Cleaning

--- merge-sort/main.cpp
+++ merge-sort/main.cpp
@@ -11,68 +11,6 @@ void printArray(std::vector<int> &arr)
     std::cout << "]" << std::endl;
 }
 
-/* void merge(std::vector<int> &arr, int p, int q, int r)
-{
-    int n1 = q - p;
-    int n2 = r - q;
-
-    std::vector<int> L(n1 + 1);
-    std::vector<int> R(n2 + 1);
-
-    for (size_t i = 0; i < L.size() - 1; i++)
-        L[i] = arr[p + i];
-
-    for (size_t i = 0; i < R.size(); i++)
-        R[i] = arr[q + i];
-
-    L[n1] = INT32_MAX;
-    R[n2] = INT32_MAX;
-
-    int i = 0;
-    int j = 0;
-
-    for (size_t k = p; k < r; k++)
-    {
-        if (L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            ++i;
-        }
-        else
-        {
-            arr[k] = R[j];
-            ++j;
-        }
-    }
-}
-
-void mergeSort(std::vector<int> &arr, int p, int r)
-{
-    if (p >= r)
-        return;
-
-    int q = (p + r) / 2;
-
-    std::cout << "q: " << q << std::endl;
-    mergeSort(arr, p, q);
-    mergeSort(arr, q + 1, r);
-    merge(arr, p, q, r);
-}
-
-int main(int argc, const char *argv[])
-{
-    std::vector<int> arr = {2, 5, 7, -1};
-    printArray(arr);
-
-
-    mergeSort(arr, 0, arr.size()-1);
-
-
-
-    printArray(arr);
-    return 0;
-} */
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
